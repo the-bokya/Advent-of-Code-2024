2ff51e88bd9f6037bae9140c11eb78b0eb0d8107
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-12-01 12:24:23 +0530

    Day 1: Part 2

---
+++ 1/2.py
@@ -0,0 +1,19 @@
+from collections import Counter
+a, b = [], []
+
+# Assuming you pipe the input. Didn't bother with file handling.
+while True:
+    try:
+        x, y = [int(i) for i in input().split()]
+        a.append(x)
+        b.append(y)
+    except:
+        break
+
+a_c, b_c = Counter(a), Counter(b)
+
+count = 0
+
+for x in a_c:
+    count += x * b_c[x]
+print(count)
