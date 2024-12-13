b1d2428f874397e834798c2e5e4b1733aadbef30
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-12-13 12:35:29 +0530

    Day 13: Part 1

---
+++ 13/1.py
@@ -0,0 +1,22 @@
+import re
+tokens = 0
+lines = []
+while True:
+    try:
+        a1, a2 = [int(i) for i in re.findall("[0-9]+", input())]
+        b1, b2 = [int(i) for i in re.findall("[0-9]+", input())]
+        c1, c2 = [int(i) for i in re.findall("[0-9]+", input())]
+        lines.append((a1, a2, b1, b2, c1, c2))
+        input()
+
+    except Exception as e:
+        break
+
+for line in lines:
+    a1, a2, b1, b2, c1, c2 = line
+    x = (b2*c1 - b1*c2) // (b2*a1 - b1*a2)
+    y = (a2*c1 - a1*c2) // (a2*b1 - a1*b2)
+
+    if (a1 * x + b1 * y == c1) and (a2 * x + b2 * y == c2) and 0 <= x <= 100 and 0 <= y <= 100:
+        tokens += 3*x + y
+print(tokens)
