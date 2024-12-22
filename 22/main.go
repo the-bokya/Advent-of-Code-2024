fee4f7c338da38f6a672e0715a7f2fcddd77aac9
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-12-22 15:14:15 +0530

    Day 22: Part 2 - Go

--- 22/main.go
+++ 22/main.go
@@ -8,6 +8,23 @@ import (
 	"strconv"
 )
 
+type Counter map[int]int
+
+type PricePriceDiff struct {
+	Price     int
+	PriceDiff int
+}
+
+func (c Counter) Add(add Counter) {
+	for k := range c {
+		c[k] += add[k]
+		delete(add, k)
+	}
+	for k := range add {
+		c[k] += add[k]
+	}
+}
+
 func main() {
 	fileName := flag.String("file", "input", "Filename of input")
 	flag.Parse()
@@ -25,6 +42,7 @@ func main() {
 		nums = append(nums, num)
 	}
 	fmt.Println("Part 1:", part1(nums))
+	fmt.Println("Part 2:", part2(nums))
 }
 
 func part1(nums []int) (count int) {
@@ -33,6 +51,21 @@ func part1(nums []int) (count int) {
 		count += genSecretNth(num, 2000)
 	}
 	return count
+}
+
+func part2(nums []int) int {
+	counter := make(Counter)
+	for _, num := range nums {
+		seq := genSecretSequence(num, 2000)
+		prices := genPrices(num, seq)
+		pmap := priceRangeMap(prices)
+		counter.Add(pmap)
+	}
+	out := 0
+	for _, v := range counter {
+		out = max(out, v)
+	}
+	return out
 }
 
 func genSecret(num int) (secret int) {
@@ -53,6 +86,7 @@ func genSecretNth(num, n int) int {
 	}
 	return currentSecret
 }
+
 func genSecretSequence(num, n int) []int {
 	secrets := make([]int, n)
 	currentSecret := num
@@ -61,4 +95,36 @@ func genSecretSequence(num, n int) []int {
 		secrets[i] = currentSecret
 	}
 	return secrets
+}
+
+func genPrices(initPrice int, nums []int) []PricePriceDiff {
+	out := make([]PricePriceDiff, len(nums))
+	prevPrice := initPrice % 10
+	for i, num := range nums {
+		var current PricePriceDiff
+		current.Price = num % 10
+		current.PriceDiff = current.Price - prevPrice
+		out[i] = current
+		prevPrice = current.Price
+	}
+	return out
+}
+
+func priceRangeMap(prices []PricePriceDiff) Counter {
+	out := make(Counter)
+	for i := range len(prices) - 3 {
+		//unique key generation through base 20 (cuz negative numbers too)
+		var key int
+		mult := 1
+		for j := range 4 {
+			key += (prices[i+j].PriceDiff + 10) * mult
+			mult *= 20
+		}
+		price := prices[i+3].Price
+		_, ok := out[key]
+		if !ok {
+			out[key] = price
+		}
+	}
+	return out
 }
